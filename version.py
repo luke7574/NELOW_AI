517ce6ce4d5a466b73b4e5bd92ecacfb2a1e4eb0
luke7574 <luke7574@naver.com>
2025-03-06 09:27:52 +0900

    Initial commit

---
+++ version.py
@@ -0,0 +1,7 @@
+import pkg_resources
+
+# 설치된 모든 패키지와 버전 출력
+installed_packages = {pkg.key: pkg.version for pkg in pkg_resources.working_set}
+
+for package, version in sorted(installed_packages.items()):
+    print(f"{package}: {version}")
